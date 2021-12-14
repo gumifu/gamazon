93c39581e4f191b8fd5a19ab3a90b46cdef850e6
gumifu <ffumi.66@gmail.com>
2021-12-14 18:28:54 +0900

    second commit gamazon

--- js/slider.js
+++ js/slider.js
@@ -2,6 +2,7 @@ $(function () {
 
     $('.slider').slick({
         autoplay: true,//自動的に動き出すか。初期値はfalse。
+        autoplaySpeed: 5000,
         infinite: true,//スライドをループさせるかどうか。初期値はtrue。
         slidesToShow: 1,//スライドを画面に3枚見せる
         slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
